4f02d806d3c7cac6a12784f99d31744fca8c67ac
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2024-04-25 02:16:34 +0500

    String methods

---
+++ main.js
@@ -0,0 +1,152 @@
+///////////string method
+
+
+// function counter (name,birthYear){
+//     return  `${"name" + " " + name} hozir  ${2023 - birthYear} yoshda.`
+// }
+
+// console.log(counter("ilyos",2002))
+
+
+
+
+///////// string lenth
+// let fullName = "Ilyos programmer";
+
+// console.log(fullName.length);
+
+
+
+
+///////// string counter
+
+
+// let fullName = "Ilyos";
+
+// console.log(fullName[0]);
+
+// console.log(fullName[4]);
+
+// console.log(fullName[fullName.length - 1])
+
+
+/// 
+
+
+// function convert (letters){
+//     const array = [];
+//     array.push(letters[0].char)
+// }
+
+
+
+
+
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+//   }
+  
+//   for (let el of str) {
+//       console.log(el)
+//   }
+  
+//   for (let i in str) {
+//       console.log(i)
+//   }
+
+               
+
+
+
+// function loopString(letters) {
+//     const array = [0];
+//     for (const let l of letters) {
+        
+//     }
+// }
+
+
+/////////////Spread operator
+
+         //   let str = "Ilyos";
+
+                        // let arr = [...str];
+
+                        // console.log(arr);
+
+
+
+
+
+
+
+
+                        //////////////////////////////////////////////////////////////////////
+
+                        ////////////////////////////////HOMEWORK/////////////////////////////
+
+
+
+//~ 1. String holda raqamlar beriladi va uni teskari arrayga aylantiruvchi funksiya tuzing. Misol uchun: "34567" => [7, 6, 5, 4, 3]
+
+
+// 2. String qabul qiluvchi funksiya yarating va u stringni nechta ! belgisi bo’lsa hammasini olib tashlagan holda qaytarsin
+
+
+// 3. Ikki array qabul qiladigan funksiya yasang va u funksiya ikki arrayni ichidagi raqamlarini yig’indisini qaytarsin.
+
+
+// 4. Array qabul qiluvchi funksiya yarating va u funksiya arrayni ichidagi elementlarini teskari qilsin
+
+
+// 5. Arrayni qabul qiladigan va yangi arrayda qiymatlar turlarini (ma’lumotlar turlarini) qaytaruvchi funksiya yarating:
+
+//   arrayValuesTypes([1, 2, "null", []])
+// ➞ ["number", "number", "string", "object"]
+
+//   arrayValuesTypes(["214", true, false, 2, 2.15, [], null])
+//   ➞ ["string", "boolean", "boolean", "number", "number", "object", "object"]
+
+// 6. Strinlar array'ini qabul qiladigan, va ularni oddiy sonlarga aylantiradigan funksiya yarating:
+
+//   toNumberArray(["9.4", "4.2"]) ➞ [9.4, 4.2]
+
+//   toNumberArray(["91", "44"]) ➞ [91, 44]
+
+//   toNumberArray(["9.5", "8.8"]) ➞ [9.5, 8.8]
+
+
+// 7. String.length dan fondalanmasdan stringni uzunligini qaytaradigan funksiya yarating:
+
+//   length("Hello World") ➞ 11
+
+//   length("Edabit") ➞ 6
+
+//   length("wash your hands!") ➞ 16
+
+
+// 8. Funskiya array qabul qiladi va ichidagi sonlarni 2 ga kopaytiradi:
+
+//   getMultipliedArr([2, 5, 3]) ➞ [4, 10, 6]
+
+//   getMultipliedArr([1, 86, -5]) ➞ [2, 172, -10]
+
+//   getMultipliedArr([5, 382, 0]) ➞ [10, 764, 0]
+
+
+// 9. So‘zlar array'ini oladigan va uni har bir so‘z uzunligidagi arrayga aylantiruvchi funksiya yarating:
+
+//   wordLengths(["hello", "world"]) ➞ [5, 5]
+
+//   wordLengths(["Halloween", "Thanksgiving", "Christmas"]) ➞ [9, 12, 9]
+
+//   wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"]) ➞ [3, 5, 9, 4, 2, 3, 8]
+
+// 10. Raqamlar yoki harflar array'ini oladigan va stringlar array'ini qaytaruvchi funksiya yarating:
+
+//   arrayToString([1, 2, 3, 4, 5, 6]) ➞ "123456"
+
+//   arrayToString(["a", "b", "c", "d", "e", "f"]) ➞ "abcdef"
+
+//   arrayToString([1, 2, 3, "a", "s", "dAAAA"]) ➞ "123asdAAAA"
+
